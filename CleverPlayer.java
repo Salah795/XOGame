e239e324258aa7b09ece021ab7659422fd4b16f4
Salah795 <salah.taher4@gmail.com>
2025-04-19 16:20:26 +0300

    working on the playTurn() method for the CleverPlayer class.

--- CleverPlayer.java
+++ CleverPlayer.java
@@ -1,17 +1,35 @@
+import java.util.Random;
+
 public class CleverPlayer implements Player {
     private static final int DEFAULT_WIN_STREAK = 3;
     private static final int COORDINATE_LENGTH = 2;
 
     private int[][] chosenCoordinates;
+    private int currentStreakLength;
+    private Random random;
+
     public CleverPlayer() {
+        this.random = new Random();
+        this.currentStreakLength = 0;
         this.chosenCoordinates = new int[DEFAULT_WIN_STREAK][];
-        for (int index = 0; index < DEFAULT_WIN_STREAK; index++) {
-            this.chosenCoordinates[index] = new int[COORDINATE_LENGTH];
-        }
     }
 
     @Override
     public void playTurn(Board board, Mark mark) {
+        switch(this.currentStreakLength) {
+            case 0:
+                int chosenRow = this.random.nextInt(board.getSize());
+                int chosenColumn = this.random.nextInt(board.getSize());
+                if(!board.putMark(mark, chosenRow, chosenColumn)) {
+                    this.playTurn(board, mark);
+                }
+                this.chosenCoordinates[this.currentStreakLength] = new int[]{chosenRow, chosenColumn};
+                this.currentStreakLength++;
+                break;
+            case 1:
+                if(this.chosenCoordinates[0][1] == 0) {
 
+                }
+        }
     }
 }
