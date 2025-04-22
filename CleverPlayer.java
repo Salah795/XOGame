d0927fe7e811fe25c113dce7f2b135984ef93faa
Salah795 <salah.taher4@gmail.com>
2025-04-22 13:29:38 +0300

    finished the explanation of the CleverPlayer class.

--- CleverPlayer.java
+++ CleverPlayer.java
@@ -1,7 +1,14 @@
 import java.util.Random;
 
 /**
- *
+ * Class for defining a clever player objects that works in a special strategy for the XO game.
+ * Also, it implements the Player interface and implements it's strategy on the method playTurn()
+ * the CleverPlayer strategy is to try to fill the first three points in the board matrix
+ * and if the opponent have been filled one of them before the clever player he will change
+ * his strategy and start to choose the points randomly.
+ * The goal of this strategy is to win at least 55% of the rounds in a XO game of 10,000 rounds
+ * with boardSize = 4 and a winStreak = 3 against an object of type WhateverPlayer.
+ * @see WhateverPlayer
  * @author Salah Mahmied.
  */
 public class CleverPlayer implements Player {
@@ -9,10 +16,24 @@ public class CleverPlayer implements Player {
 
     private Random random;
 
+    /**
+     * The default constructor for the CleverPlayer class
+     * it will initialize the random property.
+     */
     public CleverPlayer() {
         this.random = new Random();
     }
 
+    /**
+     * This method override from the Player interface it implements the CleverPlayer strategy.
+     * The strategy: to try to fill the first three points in the board matrix
+     * and if the opponent have been filled one of them before the clever player he will change
+     * his strategy and start to choose the points randomly.
+     * The goal of this strategy is to win at least 55% of the rounds in a XO game of 10,000 rounds
+     * with boardSize = 4 and a winStreak = 3 against an object of type WhateverPlayer.
+     * @param board The current state of the game board.
+     * @param mark The mark of the clever player.
+     */
     @Override
     public void playTurn(Board board, Mark mark) {
         for(int column = 0; column < DEFAULT_WIN_STREAK; column++) {
