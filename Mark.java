b0652fe766fd21bf3b1003d26a670ef43c0ea254
Salah795 <salah.taher4@gmail.com>
2025-04-22 16:17:31 +0300

    finished the explanation of the Mark enum.

--- Mark.java
+++ Mark.java
@@ -1,6 +1,15 @@
+/**
+ * Enum that defines the possible mark in the game board.
+ * The marks can be X, O, or BLANK.
+ * @author Salah Mahmied.
+ */
 public enum Mark{
     BLANK, X, O;
 
+    /**
+     * This method will return the mark of the object of type enum mark as a string.
+     * @return The string of the mark of an object from type enum mark.
+     */
     public String toString() {
         switch (this) {
             case BLANK:
